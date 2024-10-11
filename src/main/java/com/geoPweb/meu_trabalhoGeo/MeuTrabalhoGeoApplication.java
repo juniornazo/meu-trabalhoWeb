66544d04e85f967c1e22720facb1bf19b927ce3d
Skelebunny/Junior Nazo <66928852+juniornazo@users.noreply.github.com>
2024-10-11 20:57:23 -0300

    Initial commit

---
+++ src/main/java/com/geoPweb/meu_trabalhoGeo/MeuTrabalhoGeoApplication.java
@@ -0,0 +1,13 @@
+package com.geoPweb.meu_trabalhoGeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuTrabalhoGeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuTrabalhoGeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geoPweb/meu_trabalhoGeo/MeuTrabalhoGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoPweb.meu_trabalhoGeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuTrabalhoGeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
